4f586e7d50c37e7e63ad3b0ad4874b0614fc24ec
Gaurav Sharma <gouravs229@gmail.com>
2023-10-05 13:28:22 +0530

    CRUD Functionality updated

--- food/views.py
+++ food/views.py
@@ -24,25 +24,42 @@ def details(request,item_id):
 
 
 def create_item(request):
-    if request.method == "POST":
-        form = Itemform(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('food:index')
-    else:
-        form = Itemform()
-    return render(
-        request,
-        'food/item-form.html',
-        {
-            "form": form
-        }
-    )
-    # print(form.errors)
+    form = Itemform(request.POST or None)
 
-    # if form.is_valid():
-    form.save()
-    return redirect('food:index')
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
     
+    return render(request,'food/item-form.html',{'form':form})
+
+
+    # if request.method == "POST":
+    #     form = Itemform(request.POST)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('food:index')
+    # else:
+    #     form = Itemform()
+    # return render( request,'food/item-form.html',{"form": form})
+
+
+
     
-    # return render(request,'food/item-form.html',{'form':form})
+def update_item(request,id):
+    item = Item.objects.get(id=id)
+    form = Itemform(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    
+    return render(request,'food/item-form.html',{'form':form,'item':item})
+
+def delete_item(request,id):
+    item = Item.objects.get(id=id)
+
+    if request.method == "POST":
+        item.delete()
+        return redirect('food:index')
+    
+    return render(request,'food/item-delete.html',{'item':item})
